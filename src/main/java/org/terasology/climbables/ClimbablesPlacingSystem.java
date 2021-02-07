4f97b87c283ed64c5dc317052d0601973fd540fa
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-02-07 22:39:18 +0100

    feat(JOML): replace `getSecondaryPlacementDirection` call

--- src/main/java/org/terasology/climbables/ClimbablesPlacingSystem.java
+++ src/main/java/org/terasology/climbables/ClimbablesPlacingSystem.java
@@ -1,22 +1,8 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.climbables;
 
-import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.terasology.audio.AudioManager;
 import org.terasology.audio.events.PlaySoundEvent;
@@ -32,7 +18,6 @@ import org.terasology.logic.health.EngineDamageTypes;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.inventory.InventoryUtils;
 import org.terasology.logic.inventory.ItemComponent;
-import org.terasology.math.ChunkMath;
 import org.terasology.math.Side;
 import org.terasology.registry.In;
 import org.terasology.utilities.Assets;
@@ -84,8 +69,6 @@ public class ClimbablesPlacingSystem extends BaseComponentSystem {
 
         BlockItemComponent blockItem = item.getComponent(BlockItemComponent.class);
         BlockFamily type = blockItem.blockFamily;
-        //Side clamberSide = Side.TOP;
-        Side secondaryDirection = ChunkMath.getSecondaryPlacementDirection(event.getDirection(), event.getHitNormal());
 
         BlockComponent blockComponent = targetEntity.getComponent(BlockComponent.class);
         if (blockComponent == null) {
@@ -98,7 +81,7 @@ public class ClimbablesPlacingSystem extends BaseComponentSystem {
         ClamberComponent targetClamberComponent = targetEntity.getComponent(ClamberComponent.class);
 
         Vector3i climbablePlacementPos = blockComponent.getPosition(new Vector3i());
-        Block blockToPlace = type.getBlockForPlacement(new BlockPlacementData(climbablePlacementPos, Side.LEFT, new Vector3f(secondaryDirection.direction())));
+        Block blockToPlace = type.getBlockForPlacement(new BlockPlacementData(climbablePlacementPos, Side.inDirection(event.getHitNormal()), event.getDirection()));
         if (blockToPlace == null) {
             event.consume();
             return;
