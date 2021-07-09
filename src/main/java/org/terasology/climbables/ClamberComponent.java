2b0b66e8acd2bd052a8b796cbd19c85906a94c7c
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:17 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/climbables/ClamberComponent.java
+++ src/main/java/org/terasology/climbables/ClamberComponent.java
@@ -1,26 +1,13 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.climbables;
 
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.math.Side;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-public class ClamberComponent implements Component {
+public class ClamberComponent implements Component<ClamberComponent> {
 
     public PlacingMode placingMode = PlacingMode.NORMAL;
     public boolean support = false;
@@ -36,5 +23,12 @@ public class ClamberComponent implements Component {
             default:
                 return new Vector3i(0, 0, 0);
         }
+    }
+
+    @Override
+    public void copy(ClamberComponent other) {
+        this.placingMode = other.placingMode;
+        this.support = other.support;
+        this.maxPlacementDistance = other.maxPlacementDistance;
     }
 }
